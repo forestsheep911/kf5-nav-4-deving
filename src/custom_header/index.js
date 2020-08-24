535cc729e16df75acf112f7337b54702bbe2c4e8
bxia <bxia@cybozu.cn>
2020-08-24 17:12:57 +0800

    引入sass

--- src/custom_header/index.js
+++ src/custom_header/index.js
@@ -3,7 +3,7 @@ import 'core-js/es/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import KfiveCustomHeader from './components/KfiveCustomHeader';
-import css from './css/custom_header.css';
+import css from './css/style.scss';
 
 window.Promise = window.Promise || Promise;
 ReactDOM.render(<KfiveCustomHeader />, document.getElementById('header'));

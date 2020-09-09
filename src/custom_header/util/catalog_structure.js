b5d581466a19df2a309aa65fd9f16d34571978db
bxia <bxia@cybozu.cn>
2020-09-09 14:54:21 +0800

    kf5分类修改

--- src/custom_header/util/catalog_structure.js
+++ src/custom_header/util/catalog_structure.js
@@ -1,57 +1,57 @@
 export const catalogObject = {
-  '27412' : {id: '27412', title: '开始', selected: false, sections: {
-          '106246': {id: '106246', title: 'Getting Started', open: false, loading: false, articles: []},
-          '1125834': {id: '1125834', title: 'kintone开发者账号', open: false, loading: false, articles: []},
-          '1173107': {id: '1173107', title: 'kintone开发者演示环境', open: false, loading: false, articles: []}
-          },
-          sectionsOrder: ['106246', '1125834', '1173107']
-  },
-  '1018929': {id: '1018929', title: '行业案例', selected: false, hiddenW: 740, sections: {
-          '1074068': {id: '1074068', title: '共通', open: false, loading: false, articles: []}
-          },
-          sectionsOrder: ['1074068']
-  },
-  '27413': {id: '27413', title: '教程', selected: false, hiddenW: 800, sections: {
-          '106247': {id: '106247', title: 'kintone自定义概要', open: false, loading: false, articles: []},
+  '27413' : {id: '27413', title: '新手教程', selected: false, sections: {
+          '106247': {id: '106247', title: '新手入门', open: false, loading: false, articles: []},
           '106248': {id: '106248', title: 'kintone API入门系列', open: false, loading: false, articles: []},
           '1001894': {id: '1001894', title: 'kintone 自定义的技巧', open: false, loading: false, articles: []}
           },
           sectionsOrder: ['106247', '106248', '1001894']
   },
-  '27414': {id: '27414', title: 'API文档和SDK', selected: false, hiddenW: 1015, sections: {
+  '27414': {id: '27414', title: 'API文档', selected: false, hiddenW: 780, sections: {
           '106249': {id: '106249', title: 'kintone API指南', open: false, loading: false, articles: []},
           '106250': {id: '106250', title: 'kintone REST API', open: false, loading: false, articles: []},
           '106251': {id: '106251', title: 'kintone JavaScript API', open: false, loading: false, articles: []},
-          '1125808': {id: '1125808', title: 'cybozu User API', open: false, loading: false, articles: []},
-          '1000279': {id: '1000279', title: 'SDK/Tools', open: false, loading: false, articles: []},
-          '1000278': {id: '1000278', title: 'kintone 插件开发', open: false, loading: false, articles: []},
-          '1073865': {id: '1073865', title: 'cybozu.cn 共通', open: false, loading: false, articles:[]}
+          '1125808': {id: '1125808', title: 'cybozu User API/OAuth', open: false, loading: false, articles: []}
           },
-          sectionsOrder: ['106249', '106250', '106251', '1125808', '1000279', '1000278', '1073865']
+          sectionsOrder: ['106249', '106250', '106251', '1125808']
   },
-  '27415': {id: '27415', title: '开发范例', selected: false, hiddenW: 1140, sections: {
+  '27415': {id: '27415', title: '开发范例', selected: false, hiddenW: 875, sections: {
           '106252': {id: '106252', title: '自定义开发', open: false, loading: false, articles: []},
-          '1019421': {id: '1019421', title: '第三方API整合', open: false, loading: false, articles:[]}
+          '1019421': {id: '1019421', title: '第三方API整合', open: false, loading: false, articles: []}
           },
           sectionsOrder: ['106252', '1019421']
   },
-  '27416': {id: '27416', title: '插件', selected: false, hiddenW: 1200, sections: {
-          '1073272': {id: '1073272', title: '插件', open: false, loading: false, articles:[]}
+  '27416': {id: '27416', title: '插件', selected: false, hiddenW: 1015, sections: {
+          '1180831': {id: '1180831', title: '插件API', open: false, loading: false, articles: []},
+          '1180832': {id: '1180832', title: '插件开发', open: false, loading: false, articles: []},
+          '1073272': {id: '1073272', title: '插件范例', open: false, loading: false, articles: []}
           },
-          sectionsOrder: ['1073272']
+          sectionsOrder: ['1180831', '1180832', '1073272']
   },
-  '27417': {id: '27417', title: '工具', selected: false, hiddenW: 1300, sections: {
-          '106254': {id: '106254', title: '开发工具', open: false, loading: false, articles: []},
-          '106255': {id: '106255', title: '资源库', open: false, loading: false, articles:[]}
+  '27417': {id: '27417', title: '工具', selected: false, hiddenW: 1140, sections: {
+          '1180830': {id: '1180830', title: 'SDK', open: false, loading: false, articles: []},
+          '106254': {id: '106254', title: '开发工具', open: false, loading: false, articles:[]},
+          '106255': {id: '106255', title: '资源库', open: false, loading: false, articles:[]},
           },
-          sectionsOrder: ['106254', '106255']
+          sectionsOrder: ['1180830', '106254', '106255']
   },
-  '28043': {id: '28043', title: '通知', selected: false, hiddenW: 1300, sections: {
+  '1018929': {id: '1018929', title: '应用场景', selected: false, hiddenW: 1200, sections: {
+          '1074068': {id: '1074068', title: '共通', open: false, loading: false, articles:[]}
+          },
+          sectionsOrder: ['1074068']
+  },
+  '28043': {id: '28043', title: '通知', selected: false, hiddenW: 1290, sections: {
           '108673': {id: '108673', title: '咨询动态', open: false, loading: false, articles: []},
-          '108672': {id: '108672', title: 'API更新信息', open: false, loading: false, articles: []}
+          '108672': {id: '108672', title: 'API更新信息', open: false, loading: false, articles:[]}
           },
           sectionsOrder: ['108673', '108672']
+  },
+  '1045439': {id: '1045439', title: '账号&协议', selected: false, hiddenW: 1290, sections: {
+          '1180145': {id: '1180145', title: '协议规章', open: false, loading: false, articles: []},
+          '1180819': {id: '1180819', title: 'kintone开发者账号', open: false, loading: false, articles: []},
+          '1180820': {id: '1180820', title: 'kintone开发者演示环境', open: false, loading: false, articles: []}
+          },
+          sectionsOrder: ['1180145', '1180819', '1180820']
   }
 };
 
-export const catalogObjOrder = ['27412','1018929','27413', '27414', '27415', '27416', '27417', '28043']
+export const catalogObjOrder = ['27413','27414','27415', '27416', '27417', '1018929', '28043', '1045439']
